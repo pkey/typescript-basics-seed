7ce5c5379d3fc6331de0f32946983496978a339f
Paulius Kutka <paulius.kutka@swedbank.lt>
2018-07-02 16:28:58 +0300

    Default function parameters

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,5 @@
-const pizzas = [
-  {
-    name: "Pepperoni",
-    toppings: ["pepperoni"]
-  }
-];
+function multiply(a: any, b = 25) {
+  return a * b;
+}
 
-const mappedPizzas = pizzas.map(pizza => {
-  return pizza.name.toUpperCase();
-});
-
-const pizza = {
-  name: "Something",
-  getName: function() {
-    console.log(this.name);
-  },
-  getName2: () => console.log(pizza.name)
-};
-
-//function() does create new context while arrow function does not.
-//Only use arrow functions when you need them
-pizza.getName();
-
-pizza.name = "Something else";
-pizza.getName2();
+console.log(multiply(5));
