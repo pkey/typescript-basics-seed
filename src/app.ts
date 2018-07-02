83d62df2fb33ac1c17d66ac368dcba0cbb3c7a20
Paulius Kutka <paulius.kutka@swedbank.lt>
2018-07-02 16:25:35 +0300

    Arrow functions and implicit returns

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log("Ahoyy TypeScript!");
+const pizzas = [
+  {
+    name: "Pepperoni",
+    toppings: ["pepperoni"]
+  }
+];
+
+const mappedPizzas = pizzas.map(pizza => {
+  return pizza.name.toUpperCase();
+});
+
+const pizza = {
+  name: "Something",
+  getName: function() {
+    console.log(this.name);
+  },
+  getName2: () => console.log(pizza.name)
+};
+
+//function() does create new context while arrow function does not.
+//Only use arrow functions when you need them
+pizza.getName();
+
+pizza.name = "Something else";
+pizza.getName2();
