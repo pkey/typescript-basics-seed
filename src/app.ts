06ab83526346f90d3331b2d4a0e719add1904122
Paulius Kutka <paulius.kutka@swedbank.lt>
2018-07-02 17:33:08 +0300

    Array spread operator:

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,7 @@
-//Rest parameters. As saying - give me rest of them.
-//Can also define some additional parameters first
-function sumAll(message, ...arr) {
-  console.log(message);
-  arr.reduce((prev, next) => prev + next);
-}
+const toppings = ["bacon", "chilli"];
+const newToppings = ["pepperoni"];
 
-const sum = sumAll("Hello", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+//Spread operator creates new references to the object, so it's immutable
+const allToppings = [...toppings, ...newToppings];
 
-console.log(sum);
+console.log(allToppings);
