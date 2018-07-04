501433e933a25e206880bc03a1e89caf73c291e1
Paulius Kutka <paulius.kutka@swedbank.lt>
2018-07-04 12:06:14 +0300

    Void Type

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,20 @@
-let implicitCoupon = "pizza25";
-let explicitCoupon: string = "pizza25";
+let selectedTopping: string = "pepperoni";
+
+//Pure functions - not mutate any value outside itself
+//This one inpure - as it modifies what is outside of the function.
+
+//function returns void type - that is nothing is returned;
+//We don't need type.
+
+//Ussualy void is infered - so no need to declare it.
+function seletTopping(topping: string): void {
+  selectedTopping = topping;
+}
+
+//Void can only be assigned null or undefined
+let something: void = undefined;
+console.log(something);
+
+//selectedTopping("bacon");
+
+console.log(selectedTopping);
