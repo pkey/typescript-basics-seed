103c13a5ea8a454c8659796a9b396e3aee5550da
Paulius Kutka <paulius.kutka@swedbank.lt>
2018-07-04 11:40:17 +0300

    Implicit vs Ecplicit types

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,2 @@
-//Any type should be avoided.
-//Any type - infered type if nothing defined.
-//More liek pure javascript.
-let coupon;
-
-//Even here type is till any
-coupon = 25;
-
-//Aaand it goes...
-coupon = "25";
-
-coupon = true;
-
-//But if you add type at the beginning - no kebab;
-
-//Use cases - transition from pure javascript;
-//Or when we want to opt out of typescript
-//Typescript - makes you better;
+let implicitCoupon = "pizza25";
+let explicitCoupon: string = "pizza25";
