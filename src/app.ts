75e721feedbd451d454300c123dd6a12c5b7b63e
Paulius Kutka <paulius.kutka@swedbank.lt>
2018-07-04 12:11:54 +0300

    Never Type

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,13 @@
-let selectedTopping: string = "pepperoni";
+//Never type - value will never occur.
 
-//Pure functions - not mutate any value outside itself
-//This one inpure - as it modifies what is outside of the function.
-
-//function returns void type - that is nothing is returned;
-//We don't need type.
-
-//Ussualy void is infered - so no need to declare it.
-function seletTopping(topping: string): void {
-  selectedTopping = topping;
+//We can return never when we a re sure that the funciton will never return anything.
+function orderError(error: string): never {
+  throw new Error(error);
+  //return 0; -- can't do that
+  // never going to return a value!
 }
 
-//Void can only be assigned null or undefined
-let something: void = undefined;
-console.log(something);
+//Never means that there is unreachable code in our application. Like while true
+orderError("Something went rwing.");
 
-//selectedTopping("bacon");
-
-console.log(selectedTopping);
+//Seems that it's not often used. And also better used when throwing errors or sth.
