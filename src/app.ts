300b2a6ce102e7c788ce2e090e990fcf2d5cb9fc
Paulius Kutka <paulius.kutka@swedbank.lt>
2018-08-03 15:26:24 +0300

    Functions and Optional Arguments

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
-let sumOrder: Function;
+//More readable approach to let typescript compiler know that we did this intentionaly
+let sumOrder: (price: number, quantity?: number) => number;
 
-sumOrder = (price: number, quantity: number): number => {
-  return price * quantity;
+sumOrder = (x, y) => {
+  if (y) {
+    return x * y;
+  }
+  return x;
 };
 
 const sum = sumOrder(25, 2);
