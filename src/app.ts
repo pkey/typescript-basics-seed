72abf97f49e90ec6178d51bcb22986b115b6ddb1
Paulius Kutka <paulius.kutka@swedbank.lt>
2018-08-03 15:30:28 +0300

    Typed Functions and Default Params

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 //More readable approach to let typescript compiler know that we did this intentionaly
 let sumOrder: (price: number, quantity?: number) => number;
 
-sumOrder = (x, y) => {
-  if (y) {
-    return x * y;
-  }
-  return x;
+sumOrder = (x, y = 1) => {
+  return x * y;
+  // const q = y || 1; //Or parameter assigns that one if y is undefined
+  // return x * q;
 };
 
 const sum = sumOrder(25, 2);
