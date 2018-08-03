6e9805ce4ec075525eb948334658ad8bdd554040
Paulius Kutka <paulius.kutka@swedbank.lt>
2018-08-03 15:52:50 +0300

    Tuple Types for Arrays

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-let sizes: string[];
+/*Tuples can be a good use case for Google Maps Api, or third party lib or your own api
+when you don't always get back array of objects
+*/
 
-sizes = ["small", "medium", "large"];
+//Use when you absolutely know that the data structure is going to definitely be like this.
+//It's like union type but inside the array.
+let pizza: [string, number, boolean];
 
-//You see a generic type. We can create our own generic types.
-//Using when we work with third party libs, obeservalbes, promises,
-let toppings: Array<string>;
+pizza = ["Pepperoni", 20, true];
 
-toppings = ["pepperoni", "tomato", "bacon"];
+//This is with or operator - allows more flexibility
+let something: (number | string)[];
+
+something = ["string", 2];
