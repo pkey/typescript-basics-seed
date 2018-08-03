bd42a73067e445c22e238aca5c74ce3481301cfa
Paulius Kutka <paulius.kutka@swedbank.lt>
2018-08-03 15:19:21 +0300

    Function Types

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,9 @@
-let pizzaSize: string = "small";
+let sumOrder: Function;
 
-//With a union operator you can actually define what kind of variable to you want to receive.
+sumOrder = (price: number, quantity: number): number => {
+  return price * quantity;
+};
 
-//Nothing of this gets translated to plain javascript. It's just a build step process.
-//Works with number as well.
-function selectSize(size: "small" | "medium" | "large"): void {
-  pizzaSize = size;
-}
+const sum = sumOrder(25, 2);
 
-selectSize("medium");
-
-console.log(pizzaSize);
+console.log(`Total sum: ${sum}`);
