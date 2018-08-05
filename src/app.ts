724f3d8d861ff2ffcb147c76a1bffaa24d930075
Paulius Kutka <kutka100@gmail.com>
2018-08-05 12:38:45 +0300

    Static Properties and Methods

--- src/app.ts
+++ src/app.ts
@@ -1,63 +1,18 @@
-interface SizesInterface {
-  //sizes: string[]; --> we can do this with protected -> only with public.
-  availableSizes: string[]; //No way to explicitly state that it will be setter or getter, just return value
-  //Units tests come into play here.
-}
+/*
 
-//Extends is how we can physically extends classes.
-abstract class Sizes implements SizesInterface {
-  constructor(protected sizes: string[]) {}
+//const date = +new Date(); // + converts it into a timestamp. + is an instance method.
+const date = Date.now(); // Static method.
+console.log(date);
 
-  set availableSizes(sizes: string[]) {
-    this.sizes = sizes;
-  }
+*/
 
-  get availableSizes() {
-    return this.sizes;
+//Main thing - accesing something without creating a new instance
+class Coupon {
+  static allowed = ["Pepperoni", "Blazing Inferno"];
+  static create(percentage: number) {
+    return `PIIZA_RESTAURANT_${percentage}`;
   }
 }
 
-interface PizzaInterface extends SizesInterface {
-  readonly name: string; //Can define that it is special public property radonly.
-  toppings: string[];
-  updateSizes(sizes: string[]): void;
-  addTopping(topping: string): void;
-}
-
-//Not part of the tutorial
-abstract class Toppings {
-  constructor(protected toppings: string[]) {}
-
-  set availableToppings(toppings: string[]) {
-    this.toppings = toppings;
-  }
-
-  get availableToppings() {
-    return this.toppings;
-  }
-}
-
-//Turns out you cannot extends multiple classes. This is where Mixins come in - not that convenient?
-class Pizza extends Sizes implements PizzaInterface {
-  public toppings: string[] = [];
-
-  constructor(readonly name: string, sizes: string[]) {
-    super(sizes);
-  }
-
-  public updateSizes(sizes: string[]) {
-    this.sizes = sizes;
-  }
-
-  addTopping(topping: string) {
-    this.toppings.push(topping);
-  }
-}
-
-const pizza = new Pizza("Pepperoni", ["small", "medium"]);
-
-console.log(pizza.availableSizes);
-
-pizza.updateSizes(["large"]);
-
-console.log(pizza.availableSizes);
+console.log(Coupon.allowed);
+console.log(Coupon.create(25));
