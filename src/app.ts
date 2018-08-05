fab982c519e69439487d400db53a792cb59c2858
Paulius Kutka <kutka100@gmail.com>
2018-08-05 10:42:57 +0300

    Public and Private Members

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,22 @@
+//Compiled output does not change - public/private do not exist in javascript world.
 class Pizza {
-  name: string; //You need to declare variables here in TS.
-  toppings: string[] = []; //Defaulting at the top
+  // public name: string;
+  public toppings: string[] = [];
 
-  constructor(name: string) {
-    this.name = name;
+  constructor(private name: string) {
+    // sugar syntax to do what was written before. Can also be public.
+    //this.name = name;
   }
 
+  //By default methods are public in typescript
   addTopping(topping: string) {
     this.toppings.push(topping);
   }
 }
 
-/* Classical way of doing thing
-
-function Pizza(name: string) {
-  this.name = name; //Works when disabling strict.
-  this.toppings = [];
-}
-
-//Possible to add functions on a prototype
-Pizza.prototype.addTopping = function addTopping(topping: string) {
-  this.toppings.push(topping);
-};
-
-*/
-
 const pizza = new Pizza("Pepperoni");
 
+//pizza.name - would be accesable in JS. So it's may only cause build time errors.
 pizza.addTopping("pepperoni");
 
 console.log(pizza);
