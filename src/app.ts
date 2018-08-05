0b1f574163503cb04072c50cee453c5171b51614
Paulius Kutka <kutka100@gmail.com>
2018-08-05 11:44:41 +0300

    Setters and Getters (Accessors)

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,36 @@
+class Sizes {
+  //So it works with privage as well
+  constructor(private sizes: string[]) {}
+
+  //Importants setters and getters automatically are invoked when specific values is set somewhere.
+
+  //Setters and getters are always public;
+  //Setters may only have one parameter and assign one thing.
+  //The exact same name need to be used in the code
+  set availableSizes(sizes: string[]) {
+    console.log("setter was called");
+    this.sizes = sizes;
+  }
+
+  //Javacsript compiles these functions on prototype
+  //And it automatically binds setter/getter under the same name on the prototype.
+  get availableSizes() {
+    return this.sizes;
+  }
+}
+
+const sizes = new Sizes(["small", "medium"]);
+//invoke getter
+console.log(sizes.availableSizes); //Name must correspond to the getter/setter name
+
+//invoke setter
+sizes.availableSizes = ["medium", "large"];
+
+console.log(sizes.availableSizes);
+
 class Pizza {
   public toppings: string[] = [];
 
-  //Does not affect javascript output
-  //Readonly can only declared on initialisation or declarations and cannot be changed later
   constructor(readonly name: string) {}
 
   addTopping(topping: string) {
@@ -13,7 +41,3 @@ class Pizza {
 const pizza = new Pizza("Pepperoni");
 
 pizza.addTopping("pepperoni");
-
-//pizza.name = 'ABC' ---> cannot assign but can read.
-console.log(pizza.name);
-console.log(pizza);
